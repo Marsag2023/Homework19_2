570cf14d47ded95d632547f73e376fe900d39592
Marsag2023 <m.aygistov@gmail.com>
2024-05-27 13:53:02 +0300

    A fix has been made to resolve the verification error.

--- users/views.py
+++ users/views.py
@@ -33,7 +33,7 @@ class UserCreateView(CreateView):
         return super().form_valid(form)
 
 
-def email_verification(token):
+def email_verification(request, token):
     user = get_object_or_404(User, token=token)
     user.is_active = True
     user.save()
